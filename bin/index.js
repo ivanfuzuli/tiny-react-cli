bfe280f1684458e21a9216f8f713a9398fa850fa
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2022-04-13 16:31:54 +0300

    feat: add custom path support

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
-const { Command } = require("commander");
+const { Command, Option } = require("commander");
 const chalk = require("chalk");
 const path = require("path");
 const fse = require("fs-extra");
@@ -9,16 +9,19 @@ console.log(chalk.red.green("Welcome to tiny react cli"));
 
 const mapping = [
   {
+    name: "index",
     filename: "../boilerplate/index.js.eta",
     outputname: "index",
     extension: "js",
   },
   {
+    name: "component",
     filename: "../boilerplate/component.js.eta",
     outputname: "{name}",
     extension: "js",
   },
   {
+    name: "test",
     filename: "../boilerplate/test.js.eta",
     outputname: "{name}.test",
     extension: "js",
@@ -27,11 +30,11 @@ const mapping = [
 
 program
   .name("react-cli")
-  .description("CLI to react components")
+  .description("CLI tool for create react components")
   .version("0.0.1");
 
-const create = (name, ext, data) => {
-  const filename = "src/components/" + name + "." + ext;
+const create = (name, ext, data, path = "src/components/") => {
+  const filename = path + "/" + name + "." + ext;
   fse
     .outputFile(filename, data)
     .then(() =>
@@ -40,18 +43,28 @@ const create = (name, ext, data) => {
     .catch((err) => console.error(err));
 };
 
-const generate = (name) => {
-  mapping.forEach((item) => {
+const generate = (name, opts) => {
+  const { t, p } = opts;
+
+  const rules = ["index", "component"];
+  if (t) {
+    rules.push("test");
+  }
+  const filtered = mapping.filter((map) => rules.includes(map.name));
+
+  filtered.forEach((item) => {
     fse
-      .readFile(
-        path.resolve(__dirname, item.filename),
-        "UTF-8"
-      )
+      .readFile(path.resolve(__dirname, item.filename), "UTF-8")
       .then((response) => {
         const data = Eta.render(response, {
-          name
+          name,
         });
-        create(item.outputname.replace(/{name}/g, name), item.extension, data);
+        create(
+          name + "/" + item.outputname.replace(/{name}/g, name),
+          item.extension,
+          data,
+          p
+        );
       });
   });
 };
@@ -61,6 +74,9 @@ program
   .alias("g")
   .description("Generate a new react component")
   .argument("string", "Component name")
+  .option("-t", "with test files!")
+  .option('-p <value>', "component's path!!")
+
   .action(generate);
 
 program.parse();
