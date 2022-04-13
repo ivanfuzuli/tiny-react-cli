00d0d9e8543a6328e03587b023b179ef7bbb5551
Can Küçükyılmaz <can.kucukyilmaz@pusula.net.tr>
2022-04-13 14:16:12 +0300

    feat: first meaningful render

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,23 @@
-console.log('hello world');
+const { Command } = require("commander");
+const chalk = require("chalk");
+const program = new Command();
+
+console.log(chalk.red.green("Welcome to tiny react cli"));
+
+program
+  .name("react-cli")
+  .description("CLI to react components")
+  .version("0.0.1");
+
+const generate = (name) => {
+  console.log(chalk.green("CREATED:") + " " + chalk.default(name));
+};
+
+program
+  .command("generate")
+  .alias("g")
+  .description("Generate a new react component")
+  .argument("string", "Component name")
+  .action(generate);
+
+program.parse();
